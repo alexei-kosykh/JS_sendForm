d6ae1dcd78964d7e15a8b1ccaad0fd120df0b50f
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-01-13 21:33:54 +0300

    v.2.0.0

--- index.js
+++ index.js
@@ -1,55 +1,99 @@
+const form = document.querySelector("#form");
 const sendBtn = document.querySelector("#sendBtn");
-const userData = {};
-sendBtn.addEventListener("click", () => {
-  const inputName = document.querySelector("#inputName");
-  if (!inputName.value) {
-    document.getElementById("inputName").style.border = "2px solid #C74A3D";
-    document.getElementById("labelName").style.color = "#C74A3D";
-    document.getElementById("labelNameCheck").style.display = "block";
-    document.getElementById("labelNameCheck").style.marginBottom = "20px";
-    document.getElementById("inputName").style.marginBottom = "0";
-    setTimeout(() => {
-      document.getElementById("inputName").style.border = "";
-      document.getElementById("labelName").style.color = "";
-      document.getElementById("labelNameCheck").style.display = "";
-      document.getElementById("labelNameCheck").style.marginBottom = "0";
-      document.getElementById("inputName").style.marginBottom = "20px";
-    }, 2000);
+
+const userAge = document.querySelector("#userAge");
+
+const modal = document.querySelector(".modal");
+const modalBody = document.querySelector(".modal__body");
+const btnCloseModal = document.querySelector("#btnCloseModal");
+
+const openModal = () => (modal.style.display = "block");
+
+const closeModal = () => {
+  modal.style.display = "none";
+  form.reset();
+};
+
+const showInfo = (name, age, date, education, gender) => {
+  modalBody.innerHTML = `<p> Ваше имя: ${name} </p>
+  <p> Возраст: ${age} </p>
+  <p> Дата рождения: ${date} </p>
+  <p> Образование: ${education} </p>
+  <p> Пол: ${gender} </p>`;
+};
+
+const validation = (element, label, error) => {
+  if (!element.value) {
+    element.style.outline = "2px solid #C74A3D";
+    element.style.marginBottom = "0";
+
+    error.style.display = "block";
+    error.style.marginBottom = "20px";
+
+    label.style.color = "#C74A3D";
+
+    return false;
+  }
+
+  return true;
+};
+
+sendBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const userName = document.querySelector("#userName");
+  const userDay = document.querySelector("#userDay");
+  const userEducation = document.querySelector("#userEducation");
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+
+  const labelName = document.querySelector("#labelName");
+  const labelNameCheck = document.querySelector("#labelNameCheck");
+
+  if (validation(userName, labelName, labelNameCheck)) {
+    userName.style.outline = "none";
+    userName.style.marginBottom = "20px";
+
+    labelName.style.color = "none";
+
+    labelNameCheck.style.marginBottom = "0";
+    labelNameCheck.style.display = "none";
+
+    openModal();
+    showInfo(
+      userName.value,
+      userAge.value,
+      userDay.value,
+      userEducation.value,
+      gender
+    );
   } else {
-    userData.name = inputName.value; // сохранение значения
-    inputName.value = "";
+    setTimeout(() => {
+      userName.style.outline = "none";
+      userName.style.marginBottom = "20px";
 
-    const inputAge = document.querySelector("#inputAge");
-    userData.age = Number(inputAge.value);
-    inputAge.value = "";
+      labelName.style.color = "";
 
-    const inputDay = document.querySelector("#inputDay");
-    userData.day = inputDay.value;
-    inputDay.value = "";
-
-    const inputEducation = document.querySelector("#inputEducation");
-    userData.education = inputEducation.value;
-
-    const inputGender = document.querySelector("#genderChoice1");
-    if (inputGender.checked) {
-      userData.gender = document.blank.gender[0].value;
-    } else {
-      userData.gender = document.blank.gender[1].value;
-      inputGender.checked = true;
-    }
-    console.log(userData);
-
-    const bodyModal = document.querySelector("#bodyModal");
-    for (var data in userData) {
-      data = `<p> ${data}: ${userData[data]} </p>`;
-      bodyModal.insertAdjacentHTML("beforeend", data);
-    }
-
-    const modalContent = document.querySelector("#modal");
-    modalContent.hidden = false;
-    const btnCloseModal = document.querySelector("#btnCloseModal");
-    btnCloseModal.addEventListener("click", () => {
-      modalContent.hidden = true;
-    });
+      labelNameCheck.style.marginBottom = "0";
+      labelNameCheck.style.display = "none";
+    }, 2000);
   }
 });
+
+btnCloseModal.addEventListener("click", () => {
+  closeModal();
+  form.reset();
+});
+
+modal.addEventListener("click", (event) => {
+  if (!event.target.closest(".modal__wrapper")) {
+    closeModal();
+  }
+});
+
+userAge.onkeypress = (event) => {
+  if (event.keyCode < 48 || event.keyCode > 57) {
+    return false;
+  } else {
+    userAge.style.outline = "1px solid green";
+  }
+};
